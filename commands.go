c361866e6e7c089f804e2069cc4fbf3a62c5aff0
Seth Hoenig <shoenig@duck.com>
2024-08-15 20:23:25 -0500

    components: ensure commands have a lengthy name

--- commands.go
+++ commands.go
@@ -130,21 +130,6 @@ func (c *Component) init() {
 	}
 }
 
-func (c *Component) validate(output io.Writer) bool {
-	ok := true
-	for _, f := range c.Flags {
-		if len(f.Long) == 1 {
-			writef(output, "babycli: long flag %q must be more than one character", f.Long)
-			ok = false
-		}
-		if len(f.Short) > 1 {
-			writef(output, "babycli: short flag %q must be one character", f.Short)
-			ok = false
-		}
-	}
-	return ok
-}
-
 func (c *Component) run(output io.Writer) *result {
 	c.init()
 
--- validation_test.go
+++ validation_test.go
@@ -58,3 +58,55 @@ func TestComponent_validate_long_flag(t *testing.T) {
 	message := strings.TrimSpace(w.String())
 	must.Eq(t, `babycli: long flag "x" must be more than one character`, message)
 }
+
+func TestComponent_validate_name_empty(t *testing.T) {
+	t.Parallel()
+
+	config := &Configuration{
+		Top: &Component{
+			Components: Components{
+				{
+					Name: "first",
+				},
+				{
+					Name: "",
+				},
+			},
+		},
+	}
+
+	w := new(bytes.Buffer)
+	c := New(config)
+	c.output = w
+
+	result := c.Run()
+	must.One(t, result)
+	message := strings.TrimSpace(w.String())
+	must.Eq(t, `babycli: component name missing`, message)
+}
+
+func TestComponent_validate_name_single(t *testing.T) {
+	t.Parallel()
+
+	config := &Configuration{
+		Top: &Component{
+			Components: Components{
+				{
+					Name: "first",
+				},
+				{
+					Name: "x",
+				},
+			},
+		},
+	}
+
+	w := new(bytes.Buffer)
+	c := New(config)
+	c.output = w
+
+	result := c.Run()
+	must.One(t, result)
+	message := strings.TrimSpace(w.String())
+	must.Eq(t, `babycli: component "x" must be more than one character`, message)
+}
